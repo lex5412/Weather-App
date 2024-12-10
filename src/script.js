42f8d8046439b903076c9df8d2b5efc08cf1682f
lex5412 <apommells@hotmail.com>
2024-12-09 23:10:51 -0500

    added axios and js functions to update city and temp

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+ let temperature=response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+function searchCity(city) {
+  let apiKey = "316bof0093dffa4f62eb0cbccct9293c";
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
