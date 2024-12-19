7d6a68ca683cca923d2c0b6cd2807e02be6d8c53
lex5412 <apommells@hotmail.com>
2024-12-18 21:28:59 -0500

    added axios

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
- let temperature=response.data.temperature.current;
+  let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
+}
 
+// Define searchCity and handleSearchSubmit outside of refreshWeather
 function searchCity(city) {
-  let apiKey = "316bof0093dffa4f62eb0cbccct9293c";
-  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "316bof0093dffa4f62eb0cbccct9293c"; // Replace with your actual API key
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -17,5 +19,6 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+// Attach event listener to the form
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
